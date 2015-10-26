18e87cfd4cfcd9e940332bff6587c7eae0636f40
张德强 <952528232@qq.com>
2015-10-26 12:24:59 +0800

    creat mapDemo

---
+++ mapDemo/mapDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  mapDemo
+//
+//  Created by 张德强 on 15/10/26.
+//  Copyright © 2015年 张德强. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ mapDemo/mapDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mapDemo
+//
+//  Created by 张德强 on 15/10/26.
+//  Copyright © 2015年 张德强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
